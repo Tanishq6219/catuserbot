ac39e20d44e6b1f7069cde191846ce2f90a42ebb
Zeref <69341682+Tanishq6219@users.noreply.github.com>
2021-05-28 15:37:16 +0530

    Catuserbot version 2.4.3

--- userbot/plugins/jeel.py
+++ userbot/plugins/jeel.py
@@ -9,4 +9,4 @@ from userbot.events import register
 @register(outgoing=True, pattern="^.jeelpatel$")
 async def jeelpatel(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("`\nA pro dev who makes custom Roms and a pro player in valorant know as jeel , the proest of em all`")
+        await e.edit("`\nA pro dev who makes custom Roms and a pro player in valorant known as jeel , the proest of em all`")
